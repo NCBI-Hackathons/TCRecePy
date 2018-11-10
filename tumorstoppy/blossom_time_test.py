e88419946b6f1e81b395f43243093b518b7e3aa6
smrk007 <smk170630@utdallas.edu>
2018-11-10 12:39:59 -0600

    Added blossom time test.

---
+++ tumorstoppy/blossom_time_test.py
@@ -0,0 +1,16 @@
+from time import time
+from distances import *
+t1=time()
+
+s1="CASSGATGREKFF"
+s2="CASSGTTFREKFF"
+
+weights=[1]*13
+
+for ii in range(0,10000000):
+    #TMP=sigmoid(np.dot(weights, np.fromiter(blosum62_score(s1,s2),int)))
+    TMP=blosum62_distance([s1], [s2], weights=weights, allowed_gaps=0)
+
+t2=time()
+
+print (t2-t1)
