51ffd077c5311f7979c2340dbd1d092a95d96137
Calvin Ross <calvinjayross@gmail.com>
2018-11-10 13:01:45 -0600

    fixed measures

--- tumorstoppy/measures.py
+++ tumorstoppy/measures.py
@@ -10,17 +10,17 @@ def error_evaluation(weights, data, gaps=0):
 
     for i in range(N1):
         for j in range(i+1,N1):
-            Mean1 += blosum62_distance(data1[i], data1[j], weights, gaps)
+            Mean1 += list(blosum62_distance(data1[i], data1[j], weights, gaps))[0]
     Mean1 /= N1*N1
 
     for i in range(N1):
         for j in range(N2):
-            Mean2 += blosum62_distance(data1[i], data2[j], weights, gaps)
+            Mean2 += list(blosum62_distance(data1[i], data2[j], weights, gaps))[0]
     Mean2 /= N1*N2
 
     for i in range(N2):
         for j in range(i+1,N2):
-            Mean3 += blosum62_distance(data2[i], data2[j], weights, gaps)
+            Mean3 += list(blosum62_distance(data2[i], data2[j], weights, gaps))[0]
     Mean3 /= N2*N2
 
     return (Mean1 + Mean3) / Mean2
