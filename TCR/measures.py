191e137509a24c0bf29187b76ccdb0ce09e0e533
Calvin Ross <calvinjayross@gmail.com>
2018-11-10 09:59:19 -0600

    touched up error_evaluation

--- TCR/measures.py
+++ TCR/measures.py
@@ -5,15 +5,7 @@ from distances import *
 
 def error_evaluation(weights, data, gaps=0):
     N1, N2 = map(len, data)
-    data = data.values()
-    M1 = sum(blosum62_distance(data[0], data[0], weights, gaps))
-    M2 = sum(blosum62_distance(data[0], data[1], weights, gaps))
-    M3 = sum(blosum62_distance(data[1], data[1], weights, gaps))
-    return N1*N2*(M1/N1**2+M3/N2**2)/M2
-
-def undefined_cost_function (tumor, nontumor, weights):
-    # tumor and nontumor are lists of strings
-    # weights is a list of floating point numbers
+    M1, M2, M3 = 0, 0, 0
     N1 = len(tumor)
     N2 = len(nontumor)
     
@@ -21,14 +13,23 @@ def undefined_cost_function (tumor, nontumor, weights):
     # Calculating M1
     for i in range(N1):
         for j in range(i+1,N1):
-            M1 += distance(tumor[i],tumor[j],weights)
+            M1 += list(blosum62_distance(data['tumorous'][i],
+                                         data['tumorous'][j],
+                                         weights, gaps))[0]
     M1 /= N1*N1
 
-    M3 = 0
-    # Calculating M3
+    for i in range(N1):
+        for j in range(N2):
+            M2 += list(blosum62_distance(data['tumorous'][i],
+                                         data['benign'][j],
+                                         weights, gaps))[0]
+    M2 /= N1*N2
+
     for i in range(N2):
         for j in range(i+1,N2):
-            M3 += distance(nontumor[i],nontumor[j],weights)
+            M3 += list(blosum62_distance(data['benign'][i],
+                                         data['benign'][j],
+                                         weights, gaps))[0]
     M3 /= N2*N2
 
     M2 = 0
