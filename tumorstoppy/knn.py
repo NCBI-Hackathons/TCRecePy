1786da073bf08ac51142a865b46a612dedb6b094
Dat Quoc Ngo <quocdat32461997@yahoo.com>
2018-11-10 11:53:07 -0600

    Update knn.py

--- tumorstoppy/knn.py
+++ tumorstoppy/knn.py
@@ -2,7 +2,9 @@ import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 def nearest_neighbor(non_cancer_data, cancer_data):
-
+    """
+    Compute the close non-cancer and cancer neighbors to the input to determine the status of the input
+    """
     #status is based on the mean of smallest elements of cancer and non_cancer groups
     #sort
     non_cancer_data = sorted(non_cancer_data)
