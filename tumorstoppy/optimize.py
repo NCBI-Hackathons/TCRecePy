ce3cb3f65be01dcc1e71faecce8073168f05a948
Calvin Ross <calvinjayross@gmail.com>
2018-11-10 10:40:22 -0600

    changed base directory name

---
+++ tumorstoppy/optimize.py
@@ -0,0 +1,12 @@
+# Optimization functions
+import numpy as np
+from scipy.optimize import minimize
+import cost_functions # Will be renamed to 'measures'
+
+# TODO
+# Load the appropriate data
+# Create a partially passed-argument-function
+# Optimize based on some randomly chosen weights
+# Celebrate
+
+
