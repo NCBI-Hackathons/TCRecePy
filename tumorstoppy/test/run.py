5b76ff971551e4e22ff307be413f1a92e91de0df
Calvin Ross <calvinjayross@gmail.com>
2018-11-10 16:31:22 -0600

    increased number of tests in run.py with adjusted weights

--- tumorstoppy/test/run.py
+++ tumorstoppy/test/run.py
@@ -3,7 +3,7 @@ import numpy as np
 import itertools as it
 from tumorstoppy import distances, measures, data, knn
 
-data_points = 100
+data_points = 979
 Data = data.Data
 cdr3 = data.CDR3_13
 cut_cdr3 = Data(
@@ -21,12 +21,15 @@ error_evaluation = measures.error_evaluation
 distance = distances.blosum62_distance
 
 #print(error_evaluation(None, cdr3['training'], verbose=True))
-weights = np.array([ 8.31503303,  6.3603994 ,  2.26271026, -1.51866793,  7.5740573 ,
+weights1 = np.array([8.31503303,  6.3603994 ,  2.26271026, -1.51866793,  7.5740573 ,
        7.66745514,  4.72895374,  1.29182651,  6.50981513,  3.79679703,
        6.12735646,  1.11650591,  1.27719528])
+weights2 = np.array([7.21814821, 5.19300206, 3.53396301, 3.07099894, 3.13081365,
+      3.36293533, 3.70343292, 2.65573896, 3.89459912, 2.69011136,
+      4.32294453, 5.33310198, 6.140785])
 
 def dist(s1, s2):
-    return distance(s1, s2, weights)
+    return distance(s1, s2, weights2)
 
 results_cancer = []
 for seq in cut_cdr3['test','tumorous']:
