df6674cb2f3dc1b9d34d33d15ba51c1f89a73d23
Calvin Ross <calvinjayross@gmail.com>
2018-11-10 16:15:36 -0600

    edited the mean nearest neighbor samples

--- tumorstoppy/knn.py
+++ tumorstoppy/knn.py
@@ -1,13 +1,12 @@
 import numpy as np
-import tensorflow as tf
-from sklearn.model_selection import train_test_split
+
 def nearest_neighbor(test, cancer_data, non_cancer_data, dist):
     """
     Compute the close non-cancer and cancer neighbors to the input to determine the status of the input
     """
     cancer_data = [dist(test, val) for val in cancer_data]
     non_cancer_data = [dist(test, val) for val in non_cancer_data]
-    cancer_neighbor = np.mean(cancer_data)
-    non_cancer_neighbor = np.mean(non_cancer_data)
+    cancer_neighbor = np.mean(sorted(cancer_data)[:5])
+    non_cancer_neighbor = np.mean(sorted(non_cancer_data)[:5])
 
     return cancer_neighbor / non_cancer_neighbor
